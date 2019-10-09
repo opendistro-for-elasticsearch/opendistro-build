f4ccf7aea60ef4778fb494ab753e4eadadb18442
Mohit Agrawal <mohit.ag0193@gmail.com>
2019-10-09 21:32:03 +0530

    Update opendistro-tar-build.sh

--- elasticsearch/linux_distributions/opendistro-tar-build.sh
+++ elasticsearch/linux_distributions/opendistro-tar-build.sh
@@ -31,28 +31,13 @@ done
 cp opendistro-tar-install.sh elasticsearch-$ES_VERSION
 mv elasticsearch-$ES_VERSION $PACKAGE-$OD_VERSION
 
-echo "validating that plugins has been installed"
-basedir=$PWD/$PACKAGE-$OD_VERSION/plugins
-arr=("$basedir/opendistro-job-scheduler" "$basedir/opendistro_alerting" "$basedir/opendistro_performance_analyzer" "$basedir/opendistro_security" "$basedir/opendistro_sql")
-for d in "${arr[@]}"; do
-    echo "$d" 
-    if [ -d "$d" ]; then
-        echo "directoy "$d" is present"
-    else
-        echo "ERROR: "$d" is not present"
-        exit 1;
-    fi
-done
-echo "validated that plugins has been installed"
-
+#
 rm -rf tarfiles
 mkdir tarfiles
 TARGET_DIR="$ROOT/tarfiles"
 tar -vczf $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz $PACKAGE-$OD_VERSION
 sha512sum $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz  > $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz.sha512
 sha512sum -c $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz.sha512
-#shasum -a 512 $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz  > $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz.sha512
 echo " CHECKSUM FILE "
 echo "$(cat $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz.sha512)"
-#shasum -a 512 -c $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz.sha512
 rm -rf $PACKAGE-$OD_VERSION
