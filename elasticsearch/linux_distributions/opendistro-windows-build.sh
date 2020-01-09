a16e856caa5b6e0766c564ba3d3c4782acae6f33
rish1397 <rishabhsrivastava1397@gmail.com>
2020-01-09 16:58:09 +0530

    Update opendistro-windows-build.sh

--- elasticsearch/linux_distributions/opendistro-windows-build.sh
+++ elasticsearch/linux_distributions/opendistro-windows-build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ES_VERSION=$(../bin/version-info --es)
 OD_VERSION=$(../bin/version-info --od)
 OD_PLUGINVERSION=$OD_VERSION.0
@@ -17,18 +19,19 @@ rm -rf elasticsearch-oss-$OD_VERSION-windows-x86_64.zip
 #Copy all the bat files in the bin directory
 BAT_FILES=`ls ./elasticsearch-oss-$OD_VERSION-windows-x86_64/bin/*.bat`
 cp BAT_FILES $TARGET_DIR/$PACKAGE-$OD_VERSION/bin
+ls -ltr $TARGET_DIR/$PACKAGE-$OD_VERSION/bin
 rm -rf ./elasticsearch-oss-$OD_VERSION-windows-x86_64
 
 #Download install4j software
-wget https://download-gcdn.ej-technologies.com/install4j/install4j_unix_4_2_8.tar.gz
+#wget https://download-gcdn.ej-technologies.com/install4j/install4j_unix_4_2_8.tar.gz
 #Untar
-tar -xzf install4j_unix_4_2_8.tar.gz
+#tar -xzf install4j_unix_4_2_8.tar.gz
 
 #Download the .install4j file from s3
-aws s3 cp s3://odfe-windows/ODFE.install4j ./
+#aws s3 cp s3://odfe-windows/ODFE.install4j ./
 
 #build the exe using install4jc
-./install4j/bin/install4jc -d ./EXE -D sourcedir=./$PACKAGE-$OD_VERSION,version=$OD_VERSION --license=L-M8-AMAZON_DEVELOPMENT_CENTER_INDIA_PVT_LTD#50047687020001-3rhvir3mkx479#484b6 ./ODFE.install4j
+#./install4j/bin/install4jc -d ./EXE -D sourcedir=./$PACKAGE-$OD_VERSION,version=$OD_VERSION --license=L-M8-AMAZON_DEVELOPMENT_CENTER_INDIA_PVT_LTD#50047687020001-3rhvir3mkx479#484b6 ./ODFE.install4j
  
  #Copy to s3
- aws s3 cp ./EXE/*.exe s3://odfe-windows/
+ #aws s3 cp ./EXE/*.exe s3://odfe-windows/
