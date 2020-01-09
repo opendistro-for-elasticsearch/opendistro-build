6392d2a0c4087f9e2d28201ce2682d61a8d04551
rish1397 <rishabhsrivastava1397@gmail.com>
2020-01-09 18:04:36 +0530

    Update opendistro-windows-build.sh

--- elasticsearch/linux_distributions/opendistro-windows-build.sh
+++ elasticsearch/linux_distributions/opendistro-windows-build.sh
@@ -5,35 +5,40 @@ OD_VERSION=$(../bin/version-info --od)
 echo "$ES_VERSION"
 echo "$OD_VERSION"
 #OD_PLUGINVERSION=$OD_VERSION.0
-#PACKAGE=opendistroforelasticsearch
-#ROOT=$(dirname "$0")
+PACKAGE=opendistroforelasticsearch
+ROOT=$(dirname "$0")
 
-#TARGET_DIR="$ROOT/tarfiles"
+TARGET_DIR="$ROOT/Windowsfiles"
+
+#Downloading tar from s3
+aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/tarball/opendistro-elasticsearch/$PACKAGE-$OD_VERSION.tar.gz $TARGET_DIR/
 #Untar the built tar artifact
-#tar -vxzf $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz
+tar -vxzf $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz
+rm -rf $TARGET_DIR/*.tar.gz
 
 #Download windowss oss for copying batch files
-#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-$OD_VERSION-windows-x86_64.zip
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-$OD_VERSION-windows-x86_64.zip -P $ROOT/
 #Unzip the oss
-#unzip elasticsearch-oss-$OD_VERSION-windows-x86_64.zip
-#rm -rf elasticsearch-oss-$OD_VERSION-windows-x86_64.zip
+unzip $ROOT/elasticsearch-oss-$OD_VERSION-windows-x86_64.zip
+rm -rf $ROOT/elasticsearch-oss-$OD_VERSION-windows-x86_64.zip
 
 #Copy all the bat files in the bin directory
-#BAT_FILES=`ls ./elasticsearch-oss-$OD_VERSION-windows-x86_64/bin/*.bat`
-#cp BAT_FILES $TARGET_DIR/$PACKAGE-$OD_VERSION/bin
+BAT_FILES=`ls $ROOT/elasticsearch-oss-$OD_VERSION-windows-x86_64/bin/*.bat`
+cp BAT_FILES $TARGET_DIR/$PACKAGE-$OD_VERSION/bin
 #ls -ltr $TARGET_DIR/$PACKAGE-$OD_VERSION/bin
-#rm -rf ./elasticsearch-oss-$OD_VERSION-windows-x86_64
+rm -rf $ROOT/elasticsearch-oss-$OD_VERSION-windows-x86_64
 
 #Download install4j software
-#wget https://download-gcdn.ej-technologies.com/install4j/install4j_unix_4_2_8.tar.gz
+wget https://download-gcdn.ej-technologies.com/install4j/install4j_unix_4_2_8.tar.gz -P $ROOT
 #Untar
-#tar -xzf install4j_unix_4_2_8.tar.gz
+tar -xzf $ROOT/install4j_unix_4_2_8.tar.gz
+rm -rf $ROOT/install4j_unix_4_2_8.tar.gz
 
 #Download the .install4j file from s3
-#aws s3 cp s3://odfe-windows/ODFE.install4j ./
+aws s3 cp s3://odfe-windows/ODFE.install4j $ROOT/
 
 #build the exe using install4jc
-#./install4j/bin/install4jc -d ./EXE -D sourcedir=./$PACKAGE-$OD_VERSION,version=$OD_VERSION --license=L-M8-AMAZON_DEVELOPMENT_CENTER_INDIA_PVT_LTD#50047687020001-3rhvir3mkx479#484b6 ./ODFE.install4j
+$ROOT/install4j/bin/install4jc -d $TARGET_DIR/EXE -D sourcedir=$TARGET_DIR/$PACKAGE-$OD_VERSION,version=$OD_VERSION --license=L-M8-AMAZON_DEVELOPMENT_CENTER_INDIA_PVT_LTD#50047687020001-3rhvir3mkx479#484b6 ./ODFE.install4j
  
  #Copy to s3
- #aws s3 cp ./EXE/*.exe s3://odfe-windows/
+ aws s3 cp $TARGET_DIR/EXE/*.exe s3://odfe-windows/
