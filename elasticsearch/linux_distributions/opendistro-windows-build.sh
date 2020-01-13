3b490331ea32c336161085ab2aeb76a2529f7c61
rish1397 <rishabhsrivastava1397@gmail.com>
2020-01-13 14:47:23 +0530

    Update opendistro-windows-build.sh

--- elasticsearch/linux_distributions/opendistro-windows-build.sh
+++ elasticsearch/linux_distributions/opendistro-windows-build.sh
@@ -10,6 +10,7 @@ TARGET_DIR="$ROOT/Windowsfiles"
 aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/tarball/opendistro-elasticsearch/$PACKAGE-$OD_VERSION.tar.gz $TARGET_DIR/
 #Untar the tar artifact
 tar -xzf $TARGET_DIR/$PACKAGE-$OD_VERSION.tar.gz --directory $TARGET_DIR
+chmod 755 $TARGET_DIR/$PACKAGE-$OD_VERSION
 rm -rf $TARGET_DIR/*.tar.gz
 
 #Remove PA plugin as it isn't provided for odfe-1.3.0 windows
@@ -29,10 +30,6 @@ BAT_FILES=`ls $ROOT/elasticsearch-$ES_VERSION/bin/*.bat`
 cp $BAT_FILES $TARGET_DIR/$PACKAGE-$OD_VERSION/bin
 rm -rf $ROOT/elasticsearch-oss-$ES_VERSION-windows-x86_64
 
-echo inside tar_dir
-ls -ltr $TARGET_DIR
-echo inside opendistrofores
-ls -ltr $TARGET_DIR/$PACKAGE-$OD_VERSION
 
 #Download install4j software
 wget https://download-gcdn.ej-technologies.com/install4j/install4j_unix_8_0_4.tar.gz -P $ROOT
@@ -42,6 +39,13 @@ rm -rf $ROOT/*tar*
 
 #Download the .install4j file from s3
 aws s3 cp s3://odfe-windows/ODFE.install4j $ROOT/
+
+echo inside root
+ls -ltr $ROOT
+echo inside tar_dir
+ls -ltr $TARGET_DIR
+echo inside opendistrofores
+ls -ltr $TARGET_DIR/$PACKAGE-$OD_VERSION
 
 #Build the exe
 $ROOT/install4j*/bin/install4jc -d $TARGET_DIR/EXE -D sourcedir=$TARGET_DIR/$PACKAGE-$OD_VERSION,version=$OD_VERSION --license=L-M8-AMAZON_DEVELOPMENT_CENTER_INDIA_PVT_LTD#50047687020001-3rhvir3mkx479#484b6 $ROOT/ODFE.install4j
