8277f4a615f1728cae9640eff7fdf13a1fffed07
rish1397 <rishabhsrivastava1397@gmail.com>
2020-01-31 01:39:07 +0530

    Update opendistro-windows-build.sh

--- elasticsearch/linux_distributions/opendistro-windows-build.sh
+++ elasticsearch/linux_distributions/opendistro-windows-build.sh
@@ -52,5 +52,5 @@ $ROOT/install4j*/bin/install4jc -d $TARGET_DIR/EXE -D sourcedir=./Windowsfiles/$
 
 #Copy to s3
 aws s3 cp $TARGET_DIR/EXE/*.exe s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/odfe-windows/odfe-executables/
-aws s3 cp $TARGET_DIR/$PACKAGE-$OD_VERSION.zip s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/odfe-windows/ode-windows-zip/
+aws s3 cp $TARGET_DIR/odfe-$OD_VERSION.zip s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/odfe-windows/ode-windows-zip/
 rm -rf ws
