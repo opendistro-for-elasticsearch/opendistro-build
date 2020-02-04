5c978f8c3afb6b9a51aa4dc29d08a286144bd1dd
rish1397 <rishabhsrivastava1397@gmail.com>
2020-02-05 02:10:50 +0530

    Update TestZIP.bat

--- elasticsearch/linux_distributions/TestZIP.bat
+++ elasticsearch/linux_distributions/TestZIP.bat
@@ -1,2 +1,17 @@
-set a=odfe
-echo %a%
+set S3_PACKAGE=odfe
+set PACKAGE=opendistroforelasticsearch
+set OD_VERSION=1.4.0
+
+echo unzipping %S3_PACKAGE%-%OD_VERSION%.zip
+unzip.\%S3_PACKAGE%-%OD_VERSION%.zip
+
+echo running es
+nohup %S3_PACKAGE%-%OD_VERSION%\%PACKAGE%-%OD_VERSION%\bin\elasticsearch.bat &
+
+echo Waiting for 30s
+ping -n 30 127.0.0.1 >.\out.txt
+echo running tests
+cd ../odfe-test/odfe-test
+dir
+pytest
+
