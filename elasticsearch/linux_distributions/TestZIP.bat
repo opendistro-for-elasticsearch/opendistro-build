b2f18feafb743f6c7ad1f81a6de81fa62845f2b8
rish1397 <rishabhsrivastava1397@gmail.com>
2020-02-05 04:10:18 +0530

    Update TestZIP.bat

--- elasticsearch/linux_distributions/TestZIP.bat
+++ elasticsearch/linux_distributions/TestZIP.bat
@@ -5,8 +5,10 @@ FOR /F "tokens=*" %%g IN ('python version-info --od') do SET OD_VERSION=%%g
 cd ..\..
 echo downloading zip from S3
 aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/odfe-windows/ode-windows-zip/%S3_PACKAGE%-%OD_VERSION%.zip .\
+dir
 echo unzipping %S3_PACKAGE%-%OD_VERSION%.zip
 unzip .\%S3_PACKAGE%-%OD_VERSION%.zip
+dir
 echo running es
 nohup .\%PACKAGE%-%OD_VERSION%\bin\elasticsearch.bat &
 echo Waiting for 30s
