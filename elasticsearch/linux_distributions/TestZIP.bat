502d5d6d1c0a447764cf58f0d515dbe9a6c5eef8
rish1397 <rishabhsrivastava1397@gmail.com>
2020-02-05 03:27:13 +0530

    Update TestZIP.bat

--- elasticsearch/linux_distributions/TestZIP.bat
+++ elasticsearch/linux_distributions/TestZIP.bat
@@ -1,18 +1,10 @@
 set S3_PACKAGE=odfe
 set PACKAGE=opendistroforelasticsearch
-cd elasticsearch\bin
-echo `python version-info --od`
-python .\version-info --od
-set OD_VERSION=`python .\version-info --od`
-echo FIRST ATTEMPT %OD_VERSION%
-FOR /F "tokens=*" %%G in ('python version-info --od') do SET OD_VERSION=%%G
-echo SECOND ATTEMPT %OD_VERSION%
+set OD_VERSION=python .\elasticsearch\bin\version-info --od
 echo unzipping %S3_PACKAGE%-%OD_VERSION%.zip
 unzip .\%S3_PACKAGE%-%OD_VERSION%.zip
-dir
 echo running es
 nohup .\%PACKAGE%-%OD_VERSION%\bin\elasticsearch.bat &
-
 echo Waiting for 30s
 ping -n 30 127.0.0.1 >.\out.txt
 echo running tests
