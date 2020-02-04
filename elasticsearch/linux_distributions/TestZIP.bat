c5445a294ad953605033794e7690f6f58e2a0eec
rish1397 <rishabhsrivastava1397@gmail.com>
2020-02-05 02:25:35 +0530

    Update TestZIP.bat

--- elasticsearch/linux_distributions/TestZIP.bat
+++ elasticsearch/linux_distributions/TestZIP.bat
@@ -6,7 +6,7 @@ echo unzipping %S3_PACKAGE%-%OD_VERSION%.zip
 unzip .\%S3_PACKAGE%-%OD_VERSION%.zip
 dir
 echo running es
-nohup .\%S3_PACKAGE%-%OD_VERSION%\%PACKAGE%-%OD_VERSION%\bin\elasticsearch.bat &
+nohup .\%PACKAGE%-%OD_VERSION%\bin\elasticsearch.bat &
 
 echo Waiting for 30s
 ping -n 30 127.0.0.1 >.\out.txt
