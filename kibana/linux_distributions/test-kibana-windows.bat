11ff3a4b7fe14421fb35481439c2ef74502179fe
rish1397 <rishabhsrivastava1397@gmail.com>
2020-02-05 15:26:22 +0530

    Update test-kibana-windows.bat

--- kibana/linux_distributions/test-kibana-windows.bat
+++ kibana/linux_distributions/test-kibana-windows.bat
@@ -8,15 +8,16 @@ set S3_PACKAGE=odfe-%OD_VERSION%-kibana
 cd ..\..
 echo downloading zip from S3
 aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/odfe-windows/ode-windows-zip/%S3_PACKAGE%.zip .\
+echo complete
 dir
 echo unzipping %S3_PACKAGE%.zip
-unzip .\%S3_PACKAGE%.zip
+#unzip .\%S3_PACKAGE%.zip
 dir
 echo running kibana
-nohup .\%PACKAGE%\bin\kibana.bat &
+#nohup .\%PACKAGE%\bin\kibana.bat &
 echo Waiting for 30s
 ping -n 30 127.0.0.1 >.\out.txt
 echo running tests
 cd ..\KibanaTest
-java -cp ".\jars\KibanaTest.jar:.\jars\selenium-java-3.141.59\*:.\jars\lib\*" org.testng.TestNG testng.xml
+#java -cp ".\jars\KibanaTest.jar:.\jars\selenium-java-3.141.59\*:.\jars\lib\*" org.testng.TestNG testng.xml
 cd ..\opendistro-build
