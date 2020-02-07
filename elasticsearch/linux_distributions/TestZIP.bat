cad8990ac46deb9bb4d556bed4b1bf2119b5de6c
rish1397 <rishabhsrivastava1397@gmail.com>
2020-02-08 02:26:23 +0530

    Update TestZIP.bat

--- elasticsearch/linux_distributions/TestZIP.bat
+++ elasticsearch/linux_distributions/TestZIP.bat
@@ -1,7 +1,7 @@
 set S3_PACKAGE=odfe
 set PACKAGE=opendistroforelasticsearch
 ::cd .\elasticsearch\bin\
-::FOR /F "tokens=*" %%g IN ('python version-info --od') do SET OD_VERSION=%%g
+FOR /F "tokens=*" %%g IN ('python version-info --od') do SET OD_VERSION=%%g
 ::cd ..\..
 ::echo downloading zip from S3
 ::aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/odfe-windows/ode-windows-zip/%S3_PACKAGE%-%OD_VERSION%.zip .\
