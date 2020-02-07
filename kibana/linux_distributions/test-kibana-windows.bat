fa9920b62c70b26ce3efcf0017d22aa0b6af9880
rish1397 <rishabhsrivastava1397@gmail.com>
2020-02-08 02:30:02 +0530

    Update test-kibana-windows.bat

--- kibana/linux_distributions/test-kibana-windows.bat
+++ kibana/linux_distributions/test-kibana-windows.bat
@@ -1,11 +1,11 @@
 set PACKAGE=opendistroforelasticsearch-kibana
-::cd .\kibana\bin\
+cd .\kibana\bin\
 ::dir
 FOR /F "tokens=*" %%g IN ('python .\version-info --od') do SET OD_VERSION=%%g
 ::echo %OD_VERSION%
 set S3_PACKAGE=odfe-%OD_VERSION%-kibana
 
-::cd ..\..
+cd ..\..
 ::echo downloading zip from S3
 ::aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/odfe-windows/ode-windows-zip/%S3_PACKAGE%.zip .\
 ::echo complete
