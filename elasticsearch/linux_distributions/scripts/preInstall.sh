7dfe644c08d02c9d74301638b2827b3397b6d37b
rish1397 <rishabhsrivastava1397@gmail.com>
2020-03-20 17:33:32 +0530

    Update and rename preInstall-rpm.sh to preInstall.sh
    The script is common for both rpm and deb systems. The script checks for the presence of kNN lib, if the lib is found it is removed then the script fetches the lib from s3 and put it in place.

--- elasticsearch/linux_distributions/scripts/preInstall.sh
+++ elasticsearch/linux_distributions/scripts/preInstall.sh
@@ -1,4 +1,10 @@
 echo "Fetching kNN lib"
+FILE=/usr/lib/libKNNIndexV1_7_3_6.so
+if [ -f "$FILE" ]
+then
+    echo "$FILE exist: removing $FILE"
+    sudo rm $FILE
+fi
 wget https://d3g5vo6xdbdb9a.cloudfront.net/downloads/k-NN-lib/libKNNIndex1_7_3_6.zip \
 && unzip libKNNIndex1_7_3_6.zip \
 && sudo mv libKNNIndexV1_7_3_6.so /usr/lib \
