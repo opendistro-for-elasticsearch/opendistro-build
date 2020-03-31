9985571316ade1ec2f6062fc95540b6d3da937e2
Rishabh Srivastava <srivasri@amazon.com>
2020-03-31 17:10:58 +0530

    Added checks for existing files

--- elasticsearch/linux_distributions/scripts/preInstall.sh
+++ elasticsearch/linux_distributions/scripts/preInstall.sh
@@ -1,5 +1,17 @@
-echo "Fetching kNN lib"
-wget https://d3g5vo6xdbdb9a.cloudfront.net/downloads/k-NN-lib/libKNNIndex1_7_3_6.zip \
-&& unzip libKNNIndex1_7_3_6.zip \
-&& sudo mv libKNNIndexV1_7_3_6.so /usr/lib \
-&& rm libKNNIndex1_7_3_6.zip
+echo "Fetching kNN library"
+FILE=/usr/lib/libKNNIndexV1_7_3_6.so
+if [ -f "$FILE" ]
+then
+    echo "$FILE exist: removing $FILE"
+    sudo rm $FILE
+fi
+FILE=/libKNNIndexV1_7_3_6.zip
+if [ -f "$FILE" ]
+then
+    echo "$FILE exist: removing $FILE"
+    sudo rm $FILE
+fi
+wget https://d3g5vo6xdbdb9a.cloudfront.net/downloads/k-NN-lib/libKNNIndexV1_7_3_6.zip \
+&& unzip libKNNIndexV1_7_3_6.zip \
+&& mv libKNNIndexV1_7_3_6.so /usr/lib \
+&& rm libKNNIndexV1_7_3_6.zip
