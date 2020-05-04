7b36abb320eaf6e31405dbdaa3cf81d82e8d1421
Rishabh Singh <rishabhksingh@gmail.com>
2020-05-04 16:15:05 +0530

    Update opendistro-windows-build.sh

--- elasticsearch/linux_distributions/opendistro-windows-build.sh
+++ elasticsearch/linux_distributions/opendistro-windows-build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-mkdir ./ws #A temporary workspace inside opendistro-build/elasticsearch/linux_distributions
-ES_VERSION=$(../bin/version-info --es)
-OD_VERSION=$(../bin/version-info --od)
+mkdir ws #A temporary workspace inside opendistro-build/elasticsearch/linux_distributionss
+cd ../bin
+ES_VERSION=$(./version-info --es)
+OD_VERSION=$(./version-info --od)
 OD_PLUGINVERSION=$OD_VERSION.0
 PACKAGE=opendistroforelasticsearch
-ROOT=$(dirname "$0")/ws
+cd ../linux_distributions
+cd ws
+echo "$ES_VERSION"
+echo "$OD_VERSION"
+ROOT=`pwd`
+echo "$ROOT"
 TARGET_DIR="$ROOT/Windowsfiles"
+mkdir -p $PACKAGE-$OD_VERSION
 
 #Download windowss oss for copying batch files
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-$ES_VERSION-windows-x86_64.zip -P $ROOT/
@@ -20,13 +27,12 @@ unzip $ROOT/elasticsearch-oss-$ES_VERSION-windows-x86_64.zip -d $ROOT
 rm -rf $ROOT/elasticsearch-oss-$ES_VERSION-windows-x86_64.zip
 
 #Install plugins
-for plugin_path in  opendistro-sql/opendistro_sql-$OD_PLUGINVERSION.zip opendistro-alerting/opendistro_alerting-$OD_PLUGINVERSION.zip opendistro-job-scheduler/opendistro-job-scheduler-$OD_PLUGINVERSION.zip opendistro-security/opendistro_security-$OD_PLUGINVERSION.zip opendistro-index-management/opendistro_index_management-$OD_PLUGINVERSION.zip
+for plugin_path in  opendistro-sql/opendistro_sql-$OD_PLUGINVERSION.zip opendistro-alerting/opendistro_alerting-$OD_PLUGINVERSION.zip opendistro-job-scheduler/opendistro-job-scheduler-$OD_PLUGINVERSION.zip opendistro-security/opendistro_security-$OD_PLUGINVERSION.zip op
 do
   $ROOT/elasticsearch-$ES_VERSION/bin/elasticsearch-plugin install --batch "https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/$plugin_path"
 done
 
-mv $ROOT/elasticsearch-$ES_VERSION $ROOT/$PACKAGE-$OD_VERSION
-cd $ROOT
+cp -r elasticsearch-$ES_VERSION/* $PACKAGE-$OD_VERSION/
 #Making zip
 zip -r odfe-$OD_VERSION.zip $PACKAGE-$OD_VERSION
 
@@ -39,7 +45,7 @@ then
   echo "Install4j not available"
   exit 1
 fi
-pwd
+#pwd
 
 #Build the exe
 install4j8.0.4/bin/install4jc -d EXE -D sourcedir=./$PACKAGE-$OD_VERSION,version=$OD_VERSION --license="L-M8-AMAZON_DEVELOPMENT_CENTER_INDIA_PVT_LTD#50047687020001-3rhvir3mkx479#484b6" ./ODFE.install4j
