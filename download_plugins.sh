2bd62e13570926d3134054368b30705442e98c3b
Peter Zhu <zhujiaxi@amazon.com>
2020-05-28 19:28:48 -0400

    Improve regex for plugin installation processes

--- download_plugins.sh
+++ download_plugins.sh
@@ -22,6 +22,8 @@ mkdir $PLUGIN_DIR
 for plugin_path in $PLUGINS
 do
   plugin_latest=`aws s3api list-objects --bucket artifacts.opendistroforelasticsearch.amazon.com --prefix "downloads/elasticsearch-plugins/${plugin_path}" --query 'Contents[].[Key]' --output text | sort | tail -n 1`
-  echo "installing $plugin_latest"
+  echo "downloading $plugin_latest"
   aws s3 cp "s3://artifacts.opendistroforelasticsearch.amazon.com/${plugin_latest}" "${PLUGIN_DIR}" --quiet
 done
+
+ls -lrt $PLUGIN_DIR
