4d33bef3744837e9e9d0b174c9a278b873d20c1f
Peter Zhu <zhujiaxi@amazon.com>
2020-06-10 19:25:18 -0400

    Small Tweaks

--- elasticsearch/linux_distributions/opendistro-tar-build.sh
+++ elasticsearch/linux_distributions/opendistro-tar-build.sh
@@ -90,7 +90,7 @@ echo "generating tar"
 tar -czf $TARGET_DIR/$PACKAGE_NAME-$OD_VERSION.tar.gz $PACKAGE_NAME-$OD_VERSION
 #tar -tavf $TARGET_DIR/$PACKAGE_NAME-$OD_VERSION.tar.gz
 cd $TARGET_DIR
-shasum -a 512 $PACKAGE_NAME-$OD_VERSION.tar.gz  > $TARGET_DIR/$PACKAGE_NAME-$OD_VERSION.tar.gz.sha512
+shasum -a 512 $PACKAGE_NAME-$OD_VERSION.tar.gz > $PACKAGE_NAME-$OD_VERSION.tar.gz.sha512
 shasum -a 512 -c $PACKAGE_NAME-$OD_VERSION.tar.gz.sha512
 echo " CHECKSUM FILE: "
 echo "$(cat $PACKAGE_NAME-$OD_VERSION.tar.gz.sha512)"
