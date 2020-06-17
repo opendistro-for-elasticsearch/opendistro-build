8c8c3a5bef492b20bc97263f37b8abc4a529f94d
Peter Zhu <zhujiaxi@amazon.com>
2020-06-17 14:10:35 -0400

    Plugin Version Opt Part2 Major IntegTest Workflows

--- .github/scripts/plugin_tag.sh
+++ .github/scripts/plugin_tag.sh
@@ -13,11 +13,13 @@ fi
 
 # This script is meant to be run within .github/scripts folder structure
 REPO_ROOT=`git rev-parse --show-toplevel`
-OD_VERSION=`$REPO_ROOT/bin/version-info --od`
 ROOT=`dirname $(realpath $0)`; cd $ROOT
 #GIT_GROUPNAME="https://github.com/opendistro-for-elasticsearch"
 GIT_BASEURL="https://github.com"
 GIT_REPONAME=$1
+
+# Avoid issues when running on windows
+OD_VERSION=`python $REPO_ROOT/bin/version-info --od`
 
 if [ "$#" -eq 2 ]
 then
@@ -26,5 +28,4 @@ fi
 
 PLUGIN_TAG=`git ls-remote --tags "${GIT_BASEURL}/${GIT_REPONAME}" v* | grep $OD_VERSION | grep -oh "v[0-9.]*" | sort | tail -n 1`
 echo "${PLUGIN_TAG}"
-
 
