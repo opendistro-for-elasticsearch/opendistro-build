f3e98202e4aaec37031ed2ed61f190a8544711dc
Weicong Sun <61702346+weicongs-amazon@users.noreply.github.com>
2020-06-17 17:50:45 -0700

    Update opendistro-tar-build.sh

--- elasticsearch/linux_distributions/opendistro-tar-build.sh
+++ elasticsearch/linux_distributions/opendistro-tar-build.sh
@@ -90,11 +90,8 @@ knnlib_latest=`aws s3api list-objects --bucket $S3_BUCKET --prefix "downloads/op
 echo "downloading $knnlib_latest"
 aws s3 cp "s3://artifacts.opendistroforelasticsearch.amazon.com/${knnlib_latest}" ./
 unzip opendistro-knnlib*.zip
-
-
-aws s3 cp s3://$S3_BUCKET/downloads/elasticsearch-plugins/opendistro-knn/libKNNIndexV1_7_3_6.so ./ \
-&& mkdir -p $PACKAGE_NAME-$OD_VERSION/plugins/opendistro-knn/knn-lib/ \
-&& mv libKNNIndexV1_7_3_6.so $PACKAGE_NAME-$OD_VERSION/plugins/opendistro-knn/knn-lib/ 
+mkdir -p $PACKAGE_NAME-$OD_VERSION/plugins/opendistro-knn/knn-lib/ 
+mv libKNNIndexV1_7_3_6.so $PACKAGE_NAME-$OD_VERSION/plugins/opendistro-knn/knn-lib/ 
 
 # Tar generation
 echo "generating tar"
