2d50b786bf3fee98aca518795849a969e4c9ce9e
Weicong Sun <61702346+weicongs-amazon@users.noreply.github.com>
2020-06-26 15:20:24 -0700

    Update opendistro-kibana-build.sh

--- kibana/linux_distributions/opendistro-kibana-build.sh
+++ kibana/linux_distributions/opendistro-kibana-build.sh
@@ -72,6 +72,7 @@ do
   $PACKAGE_NAME/bin/kibana-plugin --allow-root install "${ARTIFACTS_URL}/${plugin_latest}"
 done
 
+# Temp solution to fix the service startup issue
 cp -r $PACKAGE_NAME/plugins/opendistro_security/node_modules/brace/* $PACKAGE_NAME/node_modules/brace/
 
 # List Plugins
