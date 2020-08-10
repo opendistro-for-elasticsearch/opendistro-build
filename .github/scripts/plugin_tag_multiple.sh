96582a7f9d90e5a0be29d7b882e2a75655190676
Peter Zhu <zhujiaxi@amazon.com>
2020-08-10 17:15:45 -0400

    test versions

--- .github/scripts/plugin_tag_multiple.sh
+++ .github/scripts/plugin_tag_multiple.sh
@@ -7,8 +7,8 @@
 #
 # About:         Retrieve the latest tag of a specific repo on Git based on user parameters
 #
-# Usage:         ./plugin-tag.sh $GIT_REPONAMES [$OD_VERSION]
-#                $GIT_REPONAMES: <repo_owner>/<repo_name> (required, sep by ',')
+# Usage:         ./plugin-tag.sh $GIT_REPONAME [$OD_VERSION]
+#                $GIT_REPONAME: <repo_owner>/<repo_name> (required)
 #                $OD_VERSION: ODFE Version (optional)
 #
 # Starting Date: 2020-06-17
@@ -21,9 +21,8 @@ set -e
 if [ "$#" -eq 0 ] || [ "$#" -gt 2 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]
 then
   echo "Please assign at least 1 / at most 2 parameters when running this script"
-  echo "Example: $0 \$GIT_REPONAMES [\$OD_VERSION]"
+  echo "Example: $0 \$GIT_REPONAME [\$OD_VERSION]"
   echo "Example: $0 \"opendistro-for-elasticsearch/opendistro-build\""
-  echo "Example: $0 \"opendistro-for-elasticsearch/opendistro-build,opendistro-for-elasticsearch/sql\""
   echo "Example: $0 \"opendistro-for-elasticsearch/opendistro-build\" \"1.7.0\""
   exit 1
 fi
@@ -34,8 +33,7 @@ REPO_ROOT=`git rev-parse --show-toplevel`
 ROOT=`dirname $(realpath $0)`; cd $ROOT
 #GIT_GROUPNAME="https://github.com/opendistro-for-elasticsearch"
 GIT_BASEURL="https://github.com"
-GIT_REPONAMES=$1
-OLDIFS=$IFS
+GIT_REPONAME=$1
 
 # Avoid issues when running on windows
 OD_VERSION=`python $REPO_ROOT/bin/version-info --od`
@@ -53,10 +51,6 @@ then
 fi
 
 # Get the latest tag of the given repository
-IFS=","
-for REPO in $GIT_REPONAMES
-do
-  git ls-remote --tags "$GIT_BASEURL/$REPO" v* | grep $OD_VERSION | grep -oh "v[0-9.]*" | sort | tail -n 1
-done
-
+PLUGIN_TAG=`git ls-remote --tags "${GIT_BASEURL}/${GIT_REPONAME}" v* | grep $OD_VERSION | grep -oh "v[0-9.]*" | sort | tail -n 1`
+echo "${PLUGIN_TAG}"
 
