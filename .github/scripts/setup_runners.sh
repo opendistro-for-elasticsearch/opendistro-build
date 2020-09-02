a211d9205788dcec60387e62b29ef0e5463e447d
Peter Zhu <zhujiaxi@amazon.com>
2020-09-02 13:57:17 -0400

    test rpm runners

--- .github/scripts/setup_runners.sh
+++ .github/scripts/setup_runners.sh
@@ -80,6 +80,9 @@ GIT_URL_BASE="https://github.com"
 GIT_URL_REPO="opendistro-for-elasticsearch/opendistro-build"
 RUNNER_URL=`curl -s https://api.github.com/repos/actions/runner/releases/latest | jq -r '.assets[].browser_download_url' | grep linux-x64`
 RUNNER_DIR="actions-runner"
+RUNNER_CMD="mkdir -p '${RUNNER_DIR}' && cd '${RUNNER_DIR}' && wget -q '${RUNNER_URL}' && tar -xzf *.tar.gz \
+            && ./config.sh --unattended --url '${GIT_URL_BASE}/${GIT_URL_REPO}' --labels '${instance_name2}' --token '${instance_runner_token}' \
+            && nohup ./run.sh &"
 
 echo "###############################################"
 echo "Start Running $0 $1 $2"
@@ -122,15 +125,11 @@ then
                          --output text > /dev/null 2>&1; echo $?
 
     echo "[${instance_name2}]: Get latest runner binary to server ${RUNNER_URL}"
+    echo "[${instance_name2}]: Get runner token and bootstrap on Git"
     aws ssm send-command --targets Key=tag:Name,Values=$instance_name2 --document-name "AWS-RunShellScript" \
-                         --parameters '{"commands": ["#!/bin/bash", "sudo su - '${SETUP_AMI_USER}' -c \"mkdir -p '${RUNNER_DIR}' && cd '${RUNNER_DIR}' && wget -q '${RUNNER_URL}' && tar -xzf *.tar.gz && rm *.tar.gz \""]}' \
+                         --parameters '{"commands": ["#!/bin/bash", "sudo su - '${SETUP_AMI_USER}' -c \"'${RUNNER_CMD}' \""]}' \
                          --output text > /dev/null 2>&1; echo $?
 
-    echo "[${instance_name2}]: Get runner token and bootstrap on Git"
-    instance_runner_token=`curl --silent -H "Authorization: token ${SETUP_TOKEN}" --request POST "${GIT_URL_API}/${GIT_URL_REPO}/actions/runners/registration-token" | jq -r .token`
-    aws ssm send-command --targets Key=tag:Name,Values=$instance_name2 --document-name "AWS-RunShellScript" \
-                         --parameters '{"commands": ["#!/bin/bash", "sudo su - '${SETUP_AMI_USER}' -c \"cd '${RUNNER_DIR}' && sleep 10 && ./config.sh --unattended --url '${GIT_URL_BASE}/${GIT_URL_REPO}' --labels '${instance_name2}' --token '${instance_runner_token}' && nohup ./run.sh &\""]}' \
-                         --output text > /dev/null 2>&1; echo $?
     sleep 5
   done
 
@@ -155,6 +154,7 @@ then
   echo ""
   echo "Terminate / Delete instances and remove runners [${SETUP_INSTANCE}]"
   echo ""
+    sleep 5
 
   for instance_name3 in $SETUP_INSTANCE
   do
