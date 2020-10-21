e435b2d851b1ef5ffb19dc0f507017f4c7ae9299
sreekarjami <sreekarj@amazon.com>
2020-10-21 22:16:57 +0000

    Additional checks included in the wss-scan.sh and the workflow file has been renamed

--- standalone-tools/wss-scan.sh
+++ standalone-tools/wss-scan.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ###### Information ############################################################################
 # Name:          wss-scan.sh
 # Maintainer:    ODFE Infra Team
@@ -7,31 +8,36 @@
 #                It will scan the repositories and send the WhiteSource link to the mail 
 #                of the user. 
 #
-# Prerequisites: Need to install Java 14
+# Prerequisites: Need to install Java 11
 #                Export JAVA_HOME env variable to the JDK path
 #		 Add JAVA_HOME to PATH variable
 #                Need to set the recepient mail in wss-scan.config for local run
-#                API token is needed for local run
+#                WhiteSource API key is needed for local run, The API Key can be retrieved from the
+#                WhiteSource Admin Console of your account. Use the below command to export the API key
+#                export wss_apikey=$(APIKEY)
 #
 # Usage:         ./wss-scan.sh
 #
 # Starting Date: 10-13-2020
-# Modified Date: 10-15-2020
+# Modified Date: 10-21-2020
 ###############################################################################################
 
 set -e
 
 java -version
-if [ $? != 0 ] ; then echo "Java has not been setup" ; exit ; fi
+if [ "$?" != 0 ]
+then 
+  echo "Java has not been setup" 
+  exit 1
+fi
 
-# download the WhiteSource Agent 
-curl -LJO -sS https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
-
-# The version 20.9.2.1 has been tested and can be used if a specific version is required
-#curl -LJO -sS https://github.com/whitesource/unified-agent-distribution/releases/download/v20.9.2.1/wss-unified-agent.jar
-
-
-echo "wss-unified-agent jar download has been completed"
+if [ ! -f "wss-unified-agent.jar" ]
+then
+  # Download the WhiteSource Agent 
+  wget -q https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
+  # The version 20.9.2.1 has been tested and can be used if a specific version is required
+  #curl -LJO -sS https://github.com/whitesource/unified-agent-distribution/releases/download/v20.9.2.1/wss-unified-agent.jar
+fi
 
 # scan the config file for the user configurations
 # wss-scan.config has to be present in the same working directory as the script
@@ -42,20 +48,17 @@ gitRepos=${gitRepos//,/$'\n'}
 
 basepath=$baseDirPath"/repos"
 
-if [[ ! -e $basepath ]]; then
- mkdir -p $basepath
-fi
- 
-
 echo "Cleaning up scan directories if already present"
-rm -rf $basepath/*
+rm -rf $basepath
 
+mkdir -p $basepath
+ 
 
 # clone the desired Repos for scanning 
 for repo in $gitRepos
 do
-echo "Cloning repo "$gitBasePath$repo
-git clone "$gitBasePath$repo".git $basepath"/"$repo
+  echo "Cloning repo "$gitBasePath$repo
+  git clone "$gitBasePath$repo".git $basepath"/"$repo
 done
 
 echo -n > info.txt
@@ -63,49 +66,53 @@ echo -n > info.txt
 # scan the Repos using the WhiteSource Unified Agent
 for repo in $gitRepos
 do
-if [ -d "$basepath"/"$repo" ]
-then
-echo "Scanning repo : "$gitBasePath$repo " Project:" $repo 
-repo_path=$basepath"/"$repo
-java -jar wss-unified-agent.jar -c wss-unified-agent.config -d $repo_path -apiKey $wss_apikey -product ODFE -project $repo | grep "Project name" | sed 's/^.\{,41\}//' >> info.txt 2>&1 &
-else
-echo "Scanning failed for repo : "$gitBasePath$repo " Project:" $repo
-fi
+  repo_path=$basepath"/"$repo
+  if [ -d "$repo_path" ]
+  then
+    echo "Scanning repo: "$gitBasePath$repo " Project: " $repo 
+    java -jar wss-unified-agent.jar -c wss-unified-agent.config -d $repo_path -apiKey $wss_apikey -product ODFE -project $repo | grep "Project name" | sed 's/^.\{,41\}//' >> info.txt 2>&1 &
+  else
+    echo "Scanning failed for repo: "$gitBasePath$repo " Project: " $repo
+  fi
 done
 
 
 i=0
 # wait for the scannings to complete
-while ps ax | grep -vw grep| grep -w "wss-unified-agent.jar" > /dev/null
+while ps ax | grep -wq [w]ss-unified-agent.jar
 do
-echo "scanning is still in progress"
-sleep 60
-((i=i+1))
-# break the loop after 70 mins
-if [ $i -gt 70 ]; then break; fi
+  echo "Scanning is still in progress"
+  sleep 60
+  ((i=i+1))
+  # break the loop after 70 mins
+  if [ "$i" -gt 70 ] 
+  then
+    break 
+  fi
 done
-echo "scanning has completed"
+echo "Scanning has completed"
 
 
 # mail function to send the scan details to the desired recepient 
 mail_format_func()
 {
 
-echo "<html><body><table border=1 cellspacing=0 cellpadding=3>" > tmp.md
+echo "<html><body><table border=1 cellspacing=0 cellpadding=3>" > output.md
 while IFS= read -r line
 do
 # setting comma as the delimiter
 
-IFS=','
-read -ra val <<< "$line"
-echo "<tr>" >> tmp.md
-for ln in "${val[@]}"; do
-echo "${ln//[[:space:]]/}"
-echo "<td>"${ln//[[:space:]]/}"</td>" >> tmp.md
-done
-echo "</tr>" >> tmp.md
+  IFS=','
+  read -ra val <<< "$line"
+  echo "<tr>" >> output.md
+  for ln in "${val[@]}" 
+  do
+    echo "${ln//[[:space:]]/}"
+    echo "<td>"${ln//[[:space:]]/}"</td>" >> output.md
+  done
+  echo "</tr>" >> output.md
 done < info.txt
-echo "</table></body></html>" >> tmp.md
+echo "</table></body></html>" >> output.md
 
 }
 
@@ -116,7 +123,7 @@ mail_format_func
 #if [ "${emailid}" != "" ]; then
 #{
 #echo "Sending mail"
-#echo -e "Content-Type: text/html; charset='utf-8'\r\nSubject: ODFE Vulnerability Scan details" |cat - tmp.md |sendmail -t ${emailid}
+#echo -e "Content-Type: text/html; charset='utf-8'\r\nSubject: ODFE Vulnerability Scan details" |cat - output.md |sendmail -t ${emailid}
 #}
 #fi
 
