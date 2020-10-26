ccde698c54480d84d3b9f204622f7db8fcf390fe
Peter Zhu <zhujiaxi@amazon.com>
2020-10-26 16:08:12 -0400

    test arm64 generations

--- kibana/linux_distributions/opendistro-kibana-build-aarch64.sh
+++ kibana/linux_distributions/opendistro-kibana-build-aarch64.sh
@@ -67,7 +67,7 @@ mkdir $PACKAGE_NAME
 # Downloading Kibana oss
 echo "Downloading kibana oss"
 #curl -Ls "https://artifacts.elastic.co/downloads/kibana/kibana-oss-$ES_VERSION-linux-x86_64.tar.gz" | tar --strip-components=1 -zxf - --directory $PACKAGE_NAME
-aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/aarch64-test/kibana-${ES_VERSION}-linux-${OD_ARCH}.tar.gz . --quiet; echo $?
+aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/aarch64-test/kibana-oss-${ES_VERSION}-linux-${OD_ARCH}.tar.gz . --quiet; echo $?
 tar --strip-components=1 -zxf kibana-${ES_VERSION}-linux-${OD_ARCH}.tar.gz --directory $PACKAGE_NAME
 
 # Install required plugins
