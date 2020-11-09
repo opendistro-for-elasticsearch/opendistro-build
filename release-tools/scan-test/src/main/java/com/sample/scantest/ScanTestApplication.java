1bbad1b7b4b7feff69ea56bb6c353d96bbac908b
sreekarjami <sreekarj@amazon.com>
2020-11-09 21:24:11 +0000

    scan test to vulnerability in dependencies

---
+++ release-tools/scan-test/src/main/java/com/sample/scantest/ScanTestApplication.java
@@ -0,0 +1,13 @@
+package com.sample.scantest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanTestApplication.class, args);
+	}
+
+}
