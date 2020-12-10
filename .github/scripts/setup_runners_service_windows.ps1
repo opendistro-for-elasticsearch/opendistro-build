cd39938a2bbbc7ed0a732d06530c55ed2160ef9c
Sayali Gaikawad <gaiksaya@amazon.com>
2020-12-09 19:14:17 -0800

    final test

--- .github/scripts/setup_runners_service_windows.ps1
+++ .github/scripts/setup_runners_service_windows.ps1
@@ -83,10 +83,7 @@ if ($SETUP_ACTION -eq "--es-nosec" -Or $SETUP_ACTION -eq "--kibana-nosec"){
   del .\$PACKAGE-$OD_VERSION\config\elasticsearch.yml
   aws s3 cp s3://artifacts.opendistroforelasticsearch.amazon.com/downloads/utils/elasticsearch.yml .\$PACKAGE-$OD_VERSION\config --quiet; echo $?
   mkdir .\$PACKAGE-$OD_VERSION\snapshots
-  dir
-  pwd
-  echo "path.repo: [`"$PACKAGE-$OD_VERSION\snapshots`"]" >> .\$PACKAGE-$OD_VERSION\config\elasticsearch.yml
-  type .\$PACKAGE-$OD_VERSION\config\elasticsearch.yml
+  echo "path.repo: [\"$PACKAGE-$OD_VERSION\snapshots\"]" >> .\$PACKAGE-$OD_VERSION\config\elasticsearch.yml
 }
 
 if ($SETUP_ACTION -eq "--es-nosec"){
