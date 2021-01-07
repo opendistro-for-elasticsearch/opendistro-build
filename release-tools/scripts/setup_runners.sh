b4fa4d131786a7ae0083aac921695c1549b0d161
Peter Zhu <zhujiaxi@amazon.com>
2021-01-07 12:51:09 -0500

    Update setup_runners.sh

--- release-tools/scripts/setup_runners.sh
+++ release-tools/scripts/setup_runners.sh
@@ -100,12 +100,13 @@ SETUP_RUNNER=`echo $2 | sed 's/,/ /g'`
 SETUP_GIT_TOKEN=$3
 
 # AMI on us-west-2
-# RPM al2     x64   ec2-user ami-086e8a98280780e63 jdk14 install by workflow
-# RPM centos7 x64   centos   ami-0f539dbfee9363756 jdk14 preinstall
-# RPM centos7 arm64 centos   ami-06fea6aecc7754b17 jdk13only preinstall RH drop support
-# RPM centos8 x64   centos   ami-00eb10a0990a90cbb jdk15 preinstall
-# RPM centos8 arm64 centos   ami-09c12fde01bdedc9b jdk15 preinstall
-# DEB ubu1804 arm64 ubuntu   ami-02e560bc36d1378d1 jdk14 preinstall
+# Distros     Arch  Username AMI-ID                JDK   Comments
+# RPM-al2     x64   ec2-user ami-086e8a98280780e63 jdk14 install by workflow
+# RPM-centos7 x64   centos   ami-0f539dbfee9363756 jdk14 preinstall
+# RPM-centos7 arm64 centos   ami-06fea6aecc7754b17 jdk13 preinstall RH drop support
+# RPM-centos8 x64   centos   ami-00eb10a0990a90cbb jdk15 preinstall
+# RPM-centos8 arm64 centos   ami-09c12fde01bdedc9b jdk15 preinstall
+# DEB-ubu1804 arm64 ubuntu   ami-02e560bc36d1378d1 jdk14 preinstall
 EC2_AMI_ID=$4
 
 if [ "$SETUP_ACTION" = "run" ]
