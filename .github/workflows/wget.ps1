689fd20bc6348e8b5dbd148c06414d19299d7ae5
Sayali Gaikawad <gaiksaya@amazon.com>
2021-01-13 17:58:56 -0800

    Test

--- .github/workflows/wget.ps1
+++ .github/workflows/wget.ps1
@@ -2,6 +2,5 @@
 Invoke-WebRequest -OutFile yq_windows_386.exe https://github.com/mikefarah/yq/releases/download/v4.3.2/yq_windows_386.exe
 Get-Location
 ls
-Get-ChildItem
+dir
 Start-Process -Wait -FilePath yq_windows_386.exe -Argument "/silent" -PassThru
-yq --version
