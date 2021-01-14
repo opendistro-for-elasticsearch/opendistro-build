cf84a416ef85e3686b2061a86d798c224b93ef5e
Sayali Gaikawad <gaiksaya@amazon.com>
2021-01-14 09:35:47 -0800

    Test

--- .github/workflows/wget.ps1
+++ .github/workflows/wget.ps1
@@ -1,9 +1,4 @@
 #!/usr/bin/env pwsh
 Invoke-WebRequest -OutFile yq.exe https://github.com/mikefarah/yq/releases/download/v4.3.2/yq_windows_386.exe
 Start-Process -Wait -FilePath yq.exe -Argument "/silent" -PassThru
-pwd
-dir
-ls
-$Env:yq="$pwd\yq.exe"
-$Env:yq
-yq --version
+yq.exe --version
