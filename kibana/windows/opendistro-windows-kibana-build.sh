3251d2a4809fb0bfbc3065e749fc14fe3846b2f1
Peter Zhu <zhujiaxi@amazon.com>
2021-01-20 18:37:27 -0500

    Make windows ES and Kibana work with new staging env

--- kibana/windows/opendistro-windows-kibana-build.sh
+++ kibana/windows/opendistro-windows-kibana-build.sh
@@ -1,67 +1,97 @@
 #!/bin/bash
 set -e
 
+PLATFORM="windows"; if [ ! -z "$1" ]; then PLATFORM=$1; fi; echo PLATFORM $PLATFORM
+ARCHITECTURE="x64"; if [ ! -z "$2" ]; then ARCHITECTURE=$2; fi; echo ARCHITECTURE $ARCHITECTURE
 REPO_ROOT=`git rev-parse --show-toplevel`
 ROOT=`dirname $(realpath $0)`; echo $ROOT; cd $ROOT
-ES_VERSION=`$REPO_ROOT/bin/version-info --es`; echo $ES_VERSION
-OD_VERSION=`$REPO_ROOT/bin/version-info --od`; echo $OD_VERSION
-IS_CUT=`$REPO_ROOT/bin/version-info --is-cut`; echo IS_CUT: $IS_CUT
-S3_BUCKET="artifacts.opendistroforelasticsearch.amazon.com"
-ARTIFACTS_URL="https://d3g5vo6xdbdb9a.cloudfront.net"
+MANIFEST_FILE=$REPO_ROOT/release-tools/scripts/manifest.yml
+ES_VERSION=`$REPO_ROOT/release-tools/scripts/version-info.sh --es`; echo ES_VERSION: $ES_VERSION
+OD_VERSION=`$REPO_ROOT/release-tools/scripts/version-info.sh --od`; echo OD_VERSION: $OD_VERSION
+S3_RELEASE_BASEURL=`yq eval '.urls.ODFE.releases' $MANIFEST_FILE`
+S3_RELEASE_FINAL_BUILD=`yq eval '.urls.ODFE.releases_final_build' $MANIFEST_FILE | sed 's/\///g'`
+S3_RELEASE_BUCKET=`echo $S3_RELEASE_BASEURL | awk -F '/' '{print $3}'`
+PLUGIN_PATH=`yq eval '.urls.ODFE.releases' $MANIFEST_FILE | sed "s/^.*$S3_RELEASE_BUCKET\///g"`
+PREBUILD_URL=`yq eval ".urls.KIBANA.${PLATFORM}_${ARCHITECTURE}" $MANIFEST_FILE`
+PREBUILD_NAME=`basename $PREBUILD_URL`; echo PREBUILD_NAME $PREBUILD_NAME
+PREBUILD_NAME_NOEXT=`echo $PREBUILD_NAME | sed 's/.zip//g'`
+PACKAGE_NAME_PREBUILD=`echo $PREBUILD_NAME_NOEXT | sed 's/oss-//g'`
 PACKAGE_NAME="opendistroforelasticsearch-kibana"
-TARGET_DIR="$ROOT/target"
 
-# Please DO NOT change the orders, they have dependencies
-PLUGINS=`$REPO_ROOT/bin/plugins-info kibana windows --require-install-true`
-PLUGINS_ARRAY=( $PLUGINS )
-CUT_VERSIONS=`$REPO_ROOT/bin/plugins-info kibana cutversion --require-install-true`
-CUT_VERSIONS_ARRAY=( $CUT_VERSIONS )
+TARGET_DIR=$ROOT/target
+PLUGIN_DIR=$ROOT/plugins
 
-mkdir $TARGET_DIR
+# Set rc build
+if [ -z "$S3_RELEASE_FINAL_BUILD" ]
+then
+  S3_RELEASE_BUILD=`aws s3api list-objects --bucket $S3_RELEASE_BUCKET --prefix "${PLUGIN_PATH}${OD_VERSION}" --query 'Contents[].[Key]' --output text | awk -F '/' '{print $3}' | uniq | tail -n 1`
+  echo Latest: $S3_RELEASE_BUILD
+else
+  S3_RELEASE_BUILD=$S3_RELEASE_FINAL_BUILD
+  echo Final: $S3_RELEASE_BUILD
+fi
+
+mkdir -p $TARGET_DIR
+mkdir -p $PLUGIN_DIR
 
 # Downloading tar from s3
-aws s3 cp "s3://${S3_BUCKET}/downloads/tarball/${PACKAGE_NAME}/${PACKAGE_NAME}-${OD_VERSION}.tar.gz" . --quiet ; echo $0
+aws s3 cp "${S3_RELEASE_BASEURL}${OD_VERSION}/odfe/${PACKAGE_NAME}-${OD_VERSION}.tar.gz" . --quiet ; echo $0
 
 # Untar the tar artifact
 tar -xzf $PACKAGE_NAME-$OD_VERSION.tar.gz
 rm -rf $PACKAGE_NAME-$OD_VERSION.tar.gz
+ls -l
 
-# Re-install Windows specific version of Kibana Plugins
-for pindex in ${!PLUGINS_ARRAY[@]}
+# Please DO NOT change the orders, they have dependencies
+echo "Re-install Windows specific version of Kibana Plugins"
+PLUGINS_ARRAY=( `${REPO_ROOT}/release-tools/scripts/plugins-info.sh kibana-plugins plugin_basename` )
+
+for index in ${!PLUGINS_ARRAY[@]}
 do
-  plugin_path=${PLUGINS_ARRAY[$pindex]}
-  plugin_version=$OD_VERSION
+  plugin_latest=`(aws s3api list-objects --bucket $S3_RELEASE_BUCKET --prefix "${PLUGIN_PATH}${OD_VERSION}/${S3_RELEASE_BUILD}/kibana-plugins" --query 'Contents[].[Key]' --output text \
+                 | grep -v sha512 | grep ${PLUGINS_ARRAY[$index]} | grep zip) || (echo None)`
+  plugin_counts=`echo $plugin_latest | sed 's/.zip[ ]*/.zip\n/g' | sed '/^$/d' | wc -l`
 
-  if [ "$IS_CUT" = "true" ]
+  if [ "$plugin_counts" -gt 1 ]
   then
-    plugin_version=${CUT_VERSIONS_ARRAY[$pindex]}
+    plugin_latest=`echo $plugin_latest | sed 's/.zip[ ]*/.zip\n/g' | sed '/^$/d' | grep "$PLATFORM" | grep "$ARCHITECTURE"`
+    if [ "$plugin_latest" != "None" ]
+    then
+      echo "downloading $plugin_latest"
+      aws s3 cp "s3://${S3_RELEASE_BUCKET}/${plugin_latest}" "${PLUGIN_DIR}" --quiet; echo $?
+      plugin_tempname=`echo $plugin_latest | awk -F '/' '{print $NF}'`
+      plugin_foldername=`echo $plugin_tempname | awk -F '-' '{print $1}'`
+
+      echo "removing $plugin_foldername"
+      $PACKAGE_NAME/bin/kibana-plugin remove $plugin_foldername
+
+      echo "installing $plugin_latest"
+      $PACKAGE_NAME/bin/kibana-plugin --allow-root install "file://${PLUGIN_DIR}/${plugin_tempname}"
+    fi
   fi
 
-  if [ "$plugin_path" != "none" ]
-  then
-    plugin_name=`echo $plugin_path | awk -F '/' '{print $NF}'`
-    plugin_latest=`aws s3api list-objects --bucket $S3_BUCKET --prefix "downloads/kibana-plugins/${plugin_path}-${plugin_version}" --query 'Contents[].[Key]' --output text | sort | tail -n 1`
-    $PACKAGE_NAME/bin/kibana-plugin remove $plugin_name
-    $PACKAGE_NAME/bin/kibana-plugin --allow-root install "${ARTIFACTS_URL}/${plugin_latest}"
-  fi
 done
 
-# Download windowss oss for copying batch files
-wget -nv https://artifacts.elastic.co/downloads/kibana/kibana-oss-$ES_VERSION-windows-x86_64.zip
+# List plugins
+ls -l $PACKAGE_NAME/plugins
+
+# Download windows oss for copying batch files
+wget -nv $PREBUILD_URL
 
 # Unzip the oss
-unzip -q kibana-oss-$ES_VERSION-windows-x86_64.zip
-rm -rf kibana-oss-$ES_VERSION-windows-x86_64.zip
+unzip -q $PREBUILD_NAME
+rm -rf $PREBUILD_NAME
 
 # Copy all the bat files in the bin directory and node.exe
-BAT_FILES=`ls kibana-$ES_VERSION-windows-x86_64/bin/*.bat`
+BAT_FILES=`ls $PACKAGE_NAME_PREBUILD/bin/*.bat`
 cp -v $BAT_FILES $PACKAGE_NAME/bin
-cp -v kibana-$ES_VERSION-windows-x86_64/node/node.exe $PACKAGE_NAME/node
-rm -rf kibana-oss-$ES_VERSION-windows-x86_64
+cp -v $PACKAGE_NAME_PREBUILD/node/node.exe $PACKAGE_NAME/node
+rm -rf $PACKAGE_NAME_PREBUILD
+ls -l
 
 # Making zip
-zip -q -r $TARGET_DIR/odfe-$OD_VERSION-kibana.zip $PACKAGE_NAME
-ls -ltr
+zip -q -r $TARGET_DIR/opendistroforelasticsearch-kibana-$OD_VERSION-$PLATFORM-$ARCHITECTURE.zip $PACKAGE_NAME
+ls -l $TARGET_DIR
 
 # Download install4j software
 wget -nv https://download-gcdn.ej-technologies.com/install4j/install4j_unix_8_0_4.tar.gz
@@ -71,13 +101,13 @@ tar -xzf install4j_unix_8_0_4.tar.gz
 rm -rf install4j_unix_8_0_4.tar.gz
 
 # Download the .install4j file from s3
-aws s3 cp s3://odfe-windows/ODFE-Kibana.install4j . --quiet ; echo $0
+aws s3 cp s3://${S3_RELEASE_BUCKET}/odfe-windows/ODFE-Kibana.install4j . --quiet ; echo $0
 
- #Build the exe
+# Build the exe
 install4j8.0.4/bin/install4jc -d $TARGET_DIR -D sourcedir=./$PACKAGE_NAME,version=$OD_VERSION --license=$install4j_license ./ODFE-Kibana.install4j
-ls -ltr $TARGET_DIR
+mv -v $TARGET_DIR/*.exe $TARGET_DIR/opendistroforelasticsearch-kibana-$OD_VERSION-$PLATFORM-$ARCHITECTURE.exe
+ls -l $TARGET_DIR
 
 # Copy to s3
-aws s3 cp $TARGET_DIR/*.exe s3://$S3_BUCKET/downloads/odfe-windows/staging/odfe-executable/
-aws s3 cp $TARGET_DIR/*.zip s3://$S3_BUCKET/downloads/odfe-windows/staging/odfe-window-zip/
-aws cloudfront create-invalidation --distribution-id E1VG5HMIWI4SA2 --paths "/downloads/*"
+aws s3 cp $TARGET_DIR/*.exe ${S3_RELEASE_BASEURL}${OD_VERSION}/odfe/
+aws s3 cp $TARGET_DIR/*.zip ${S3_RELEASE_BASEURL}${OD_VERSION}/odfe/
