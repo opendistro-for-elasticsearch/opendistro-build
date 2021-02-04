71eb364ba5849884d8eef9d601994af98baf904c
Peter Zhu <zhujiaxi@amazon.com>
2021-02-04 18:08:39 -0500

    test deb

--- release-tools/scripts/setup_runners.sh
+++ release-tools/scripts/setup_runners.sh
@@ -129,6 +129,7 @@ SETUP_GIT_TOKEN=$3
 # RPM-centos8 x64   centos   ami-011f59f50bac33376 jdk15 preinstall
 # RPM-centos8 arm64 centos   ami-0ed17173ab64255b1 jdk15 preinstall
 # DEB-ubu1804 arm64 ubuntu   ami-055197d43e4ec7482 jdk14 preinstall disable daily updates
+# DEB-ubu1804 arm64 ubuntu   ami-040568c6e3d22bbe1 jdk14 preinstall + docker + docker compose
 EC2_AMI_ID=$4
 
 if [ "$SETUP_ACTION" = "run" ]
